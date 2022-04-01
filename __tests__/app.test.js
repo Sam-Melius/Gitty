5dce856d0b1cbf0b4cfa9bd05b4b402d489aeaad
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-04-01 10:14:46 -0700

    nothing is working. committing before switch to new branch to try something

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,17 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const GithubUser = require('../lib/models/GithubUser');
+
+jest.mock('../lib/middleware/authenticate.js', () => {
+  return (req, res, next) => {
+    req.user = {
+      username: 'test',
+      photoUrl: 'http://image.com/image.png'
+    };
+    next();
+  };
+});
 
 describe('Gitty routes', () => {
   beforeEach(() => {
@@ -11,12 +22,28 @@ describe('Gitty routes', () => {
   afterAll(() => {
     pool.end();
   });
+  
 
-  it('should redirect to github oauth page', async () => {
-    const req = await request(app).get('/api/v1/github/login');
-    expect(req.header.location).toMatch(
-      /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/login\/callback/i
-    );
-  });
+  
+
+  // it('creates a user', async () => {
+  //   const user = await GithubUser.insert({
+  //     username: 'wally',
+  //     photoUrl: 'http://image.com/image.png'
+  //   });
+  //   const res = await request(app)
+  //     .post('/api/v1/githubUsers/login/callback')
+  //     .send({ username: 'wally', photoUrl: 'http://image.com/image.png' });
+  //   expect(res.body).toEqual({ user });
+    
+    
+  // });
+
+  // it('should redirect to github oauth page', async () => {
+  //   const req = await request(app).get('/api/v1/github/login');
+  //   expect(req.header.location).toMatch(
+  //     /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/login\/callback/i
+  //   );
+  // });
 
 });
